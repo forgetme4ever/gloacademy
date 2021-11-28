fe5f2b0abc5637fff77cfbf0285d92887425e30b
Eugen Burkenia <burikievgen@gmail.com>
2021-11-27 23:48:46 -0500

    lesson02

--- scripts.js
+++ scripts.js
@@ -5,5 +5,23 @@ let rollback
 let fullPrice
 let adaptive
 
+
+title = 'project'
+screens = 'simple, complex, interactive'
+screenPrice = 777
+rollback = 99
+fullPrice = 1000
+adaptive = true
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log('Price layout screens ' + screenPrice + 'USD' + ' Website development cost ' + fullPrice + 'USD');
+
+screens.toLowerCase()
+
+console.log('Brokers fee ' + fullPrice * (rollback/100));
+
 alert('any text')
 console.log('other any');
