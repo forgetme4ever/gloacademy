0f54e3ab2eb644c7c22508ebf5ee3640c720ce46
Eugen Burkenia <burikievgen@gmail.com>
2021-12-01 12:25:43 -0500

    7 prompt switched to confirm, added semicolons, 35 concat was edited, 13 fixed servicePercentPrice,37 used var from 13

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 'use strict';
 
-const title = prompt("What is the name of your project?")
-const screens = prompt('What types of screens have to be developed', 'simple, complex or interactive')
-const screenPrice = +prompt('What is the price for this project?')
-const rollback = 99
-let adaptive = !!prompt('Is adaptive needed', 'yes or no')
-const service1 = prompt('Any extra services needed?')
-const service1Price = +prompt('How much it will be cost?', 'price in USD')
-const service2 = prompt('Any extra services needed?')
-const service2Price = +prompt('How much it will be cost?', 'price in USD')
-const fullPrice = screenPrice + service1Price + service2Price
-const servicePercentPrice = fullPrice - rollback
+const title = prompt("What is the name of your project?");
+const screens = prompt('What types of screens have to be developed', 'simple, complex or interactive');
+const screenPrice = +prompt('What is the price for this project?');
+const rollback = 99;
+const adaptive = confirm('Is adaptive needed');
+const service1 = prompt('Any extra services needed?');
+const service1Price = +prompt('How much it will be cost?', 'price in USD');
+const service2 = prompt('Any extra services needed?');
+const service2Price = +prompt('How much it will be cost?', 'price in USD');
+const fullPrice = screenPrice + service1Price + service2Price;
+const servicePercentPrice = fullPrice * (rollback/100);
 
 console.log(servicePercentPrice);
 
-switch(true){
+switch (true) {
 case fullPrice >= 30000:
     console.log('You are eligible for 10% discount');
-    break
+    break;
 case fullPrice >= 15000:
     console.log('You are eligible for 5% discount');
-    break
+    break;
 case fullPrice < 15000:
     console.log('You are not eligible for discount');
-    break
+    break;
 default:
     console.log('Something went wrong');  
 }
@@ -32,7 +32,7 @@ console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
-console.log('Price layout screens ' + screenPrice + 'USD' + ' Website development cost ' + fullPrice + 'USD');
+console.log('Price layout screens ' + screenPrice + 'USD website development cost ' + fullPrice + 'USD');
 console.log(screens.toLowerCase().split(','));
-console.log('Brokers fee ' + fullPrice * (rollback/100));
+console.log('Brokers fee ' + servicePercentPrice);
 console.log(adaptive);
